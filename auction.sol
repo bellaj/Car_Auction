a82887676a80045137f28628423b1aaead07381b
Badr bellaj <bellaj.badr@gmail.com>
2018-08-28 10:32:35 +0100

    update contracts

--- auction.sol
+++ auction.sol
@@ -9,7 +9,6 @@ uint256 public highestBid;
 address public highestBidder;
  
 
-
 enum auction_state{
     CANCELLED,STARTED
 }
@@ -18,34 +17,31 @@ struct  car{
     string  Brand;
     string  Rnumber;
 }
-car public Mycar;
-address[] bidders;
+    car public Mycar;
+    address[] bidders;
 
-mapping(address => uint) public bids;
+    mapping(address => uint) public bids;
 
-auction_state public STATE;
+    auction_state public STATE;
 
 
     modifier an_ongoing_auction(){
-
         require(now <= auction_end);
         _;
     }
     
     modifier only_owner(){
         require(msg.sender==auction_owner);
-        
         _;
     }
     
-    function bid() public payable {}
-    function withdraw() public {}
-    function cancel_auction() public returns (bool){}
+    function bid() public payable returns (bool){}
+    function withdraw() public returns (bool){}
+    function cancel_auction() external returns (bool){}
     
     event BidEvent(address indexed highestBidder, uint256 highestBid);
     event WithdrawalEvent(address withdrawer, uint256 amount);
-    event CanceledEvent(string message, uint256 time);
-  
+    event CanceledEvent(string message, uint256 time);  
     
 }
 
@@ -53,14 +49,13 @@ auction_state public STATE;
 contract MyAuction is Auction{
     
     
-     function ()
+     function () 
     {
         
     }
     
   
-
-    function MyAuction (uint _biddingTime, address _owner,string _brand,string _Rnumber) public {
+function MyAuction (uint _biddingTime, address _owner,string _brand,string _Rnumber) public {
         auction_owner = _owner;
         auction_start=now;
         auction_end = auction_start + _biddingTime*1  hours;
@@ -71,59 +66,56 @@ contract MyAuction is Auction{
     }
  
 
- function bid() public payable an_ongoing_auction {
+ function bid() public payable an_ongoing_auction returns (bool){
       
-
         require(bids[msg.sender]+msg.value> highestBid,"can't bid, Make a higher Bid");
-
         highestBidder = msg.sender;
         highestBid = msg.value;
         bidders.push(msg.sender);
         bids[msg.sender]=  bids[msg.sender]+msg.value;
         emit BidEvent(highestBidder,  highestBid);
 
- 
+        return true;
     }
     
  
   
-function cancel_auction() only_owner  an_ongoing_auction returns (bool){
+function cancel_auction() external only_owner  an_ongoing_auction returns (bool){
     
         STATE=auction_state.CANCELLED;
-        CanceledEvent("Auction Cancelled", now);
+        emit CanceledEvent("Auction Cancelled", now);
         return true;
      }
     
     
     
-function destruct_auction()  returns (bool){
+function destruct_auction() external returns (bool){
         
-        require(now > auction_end,"can't destruct,Auction is still open");
-     
+    require(now > auction_end,"can't destruct,Auction is still open");
      for(uint i=0;i<bidders.length;i++)
     {
         assert(bids[bidders[i]]==0);
     }
 
     selfdestruct(auction_owner);
-    
     return true;
     
     }
 
     
-        function withdraw() public {
+function withdraw() public returns (bool){
         require(now > auction_end ," can't withdraw, Auction is still open");
         uint amount;
 
         amount=bids[msg.sender];
         bids[msg.sender]=0;
         msg.sender.transfer(amount);
-        WithdrawalEvent(msg.sender, amount);
+        emit WithdrawalEvent(msg.sender, amount);
+        return true;
       
     }
     
-    function get_owner() view returns(address){
+function get_owner() public view returns(address){
         return auction_owner;
     }
 }
